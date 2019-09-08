a78685385d98ce0b9950cdd806aab4c0fda4585f
Bruno Cabezas <bruno.cabezas@gmail.com>
2019-09-07 21:03:31 -0400

    add loader to exposition views

--- src/components/Expositions/ExpositionsGrid.vue
+++ src/components/Expositions/ExpositionsGrid.vue
@@ -1,42 +1,51 @@
 <template>
-  <div class="expositionsGrid">
-    <div
-      class="exposition"
-      v-for="exposition in expositions"
-      v-bind:key="exposition.name"
-    >
-      <h3 class="expositionName">
-        <router-link :to="urls.exposition(exposition.slug)">
-          {{ exposition.name }}
-        </router-link>
-      </h3>
-      <div class="expositionText">
-        <p>{{ exposition.place }}</p>
-        <p>{{ exposition.hour }}</p>
+  <Loader :loading="loadingData">
+    <div class="expositionsGrid">
+      <div
+        class="exposition"
+        v-for="exposition in expositions"
+        v-bind:key="exposition.name"
+      >
+        <h3 class="expositionName">
+          <router-link :to="urls.exposition(exposition.slug)">
+            {{ exposition.name }}
+          </router-link>
+        </h3>
+        <div class="expositionText">
+          <p>{{ exposition.place }}</p>
+          <p>{{ exposition.hour }}</p>
+        </div>
       </div>
     </div>
-  </div>
+  </Loader>
 </template>
 
 <script>
 import { Vue, Component } from 'vue-property-decorator'
 import { getExpositionsFromApi } from '@/helpers/apiHelpers'
 import { loadExpositions } from '../../../api/client'
+import Loader from '@/components/common/Loader'
 import store from '@/config/store'
 
 @Component({
-  store
+  store,
+  components: {
+    Loader
+  }
 })
 class ExpositionsGrid extends Vue {
   urls = this.$root.urls
+  loadingData = false
 
   get expositions () {
     return this.$store.state.expositions
   }
 
   mounted () {
+    this.loadingData = true
     loadExpositions().then((response) => {
       this.$store.commit('loadExpositions', getExpositionsFromApi(response.data))
+      this.loadingData = false
     })
   }
 }
